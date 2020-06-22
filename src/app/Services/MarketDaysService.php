5237f05a6d326e02cf4e8b32f46602077a02f50a
Tsukino Usagi <tsukinousagi@users.noreply.github.com>
2020-06-22 16:53:44 +0800

    計算交易日的service

---
+++ src/app/Services/MarketDaysService.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * 查交易日相關資訊的Service
+ * Ref: https://www.twse.com.tw/zh/holidaySchedule/holidaySchedule
+ */
+namespace App\Services;
+
+use Log;
+use Exception;
+
+
+class MarketDaysService {
+    
+    /**
+     * 查詢是否為交易日
+     * @param string $d_date
+     * @return boolean
+     */
+    public function isMarketOpen(string $d_date) {
+
+        // 分離年月日
+        $d_year = substr($d_date, 0, 4);
+        $d_month = substr($d_date, 4, 2);
+        $d_day = substr($d_date, 6, 2);
+
+        // 先查是否為週六週日
+        $check_week = date('w', mktime(0, 0, 0, $d_month, $d_day, $d_year));
+        if (in_array($check_week, ['0', '6'])) {
+            return false;
+        } else {
+            // 再查是否為特定不開盤的日子
+            if ($d_year == '2020') {
+                if (in_array($d_month . $d_day, [
+                    '0101',
+                    '0121',
+                    '0122',
+                    '0123',
+                    '0124',
+                    '0125',
+                    '0126',
+                    '0127',
+                    '0128',
+                    '0129',
+                    '0228',
+                    '0402',
+                    '0403',
+                    '0404',
+                    '0501',
+                    '0625',
+                    '0626',
+                    '1001',
+                    '1002',
+                    '1009',
+                    '1010',
+                    
+                ])) {
+                    return false;
+                    
+                } else {
+                    return true;
+                }
+            }
+        }
+        
+    }
+    
+    /**
+     * 往前或往後推算交易日
+     * @param string $d_date
+     * @param string $direction
+     * @param int $days
+     * @return string
+     */
+    public function calculateMarketDays(string $d_date, string $direction, int $days) {
+        $remaining_days = $days;
+        $new_date = $d_date;
+        while ($remaining_days > 0) {
+            $new_date = $this->calculateDay($new_date, $direction);
+            if ($this->isMarketOpen($new_date)) {
+                $remaining_days--;
+            }
+        }
+        return $new_date;
+    }
+    
+    
+    /**
+     * 加減一天，不考慮交易日
+     * @param string $d_date
+     * @param string $direction
+     * @return string
+     */
+    private function calculateDay(string $d_date, string $direction) {
+        // 分離年月日
+        $d_year = substr($d_date, 0, 4);
+        $d_month = substr($d_date, 4, 2);
+        $d_day = substr($d_date, 6, 2);
+        
+        // 算timestamp
+        $ts = mktime(0, 0, 0, $d_month, $d_day, $d_year);
+        
+        // 加減
+        if ($direction == '+') {
+            $ts += 86400;
+        } else if ($direction == '-') {
+            $ts -= 86400;
+        } else {
+            return false;
+        }
+        
+        // 組回日期
+        return date('Ymd', $ts);
+    }
+
+}
---
+++ src/tests/Services/MarketDaysTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Services;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Services\MarketDaysService;
+
+class MarketDaysTest extends TestCase
+{
+    
+    /**
+     * 有開盤的日期
+     */
+    public function testOpenMarketDays() {
+        $obj = new MarketDaysService();
+        
+        $ret = $obj->isMarketOpen('20200102'); // 元旦
+        $this->assertTrue($ret);
+        $ret = $obj->isMarketOpen('20200130'); // 開紅盤
+        $this->assertTrue($ret);
+    }
+    
+    /**
+     * 不開盤的日期
+     */
+    public function testClosedMarketDays() {
+        $obj = new MarketDaysService();
+        
+        $ret = $obj->isMarketOpen('20200101'); // 元旦
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200121'); // 過年前
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200122'); // 過年前
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200123'); // 過年前
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200215'); // 補班日
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200228'); // 228
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200402'); // 清明連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200403'); // 清明連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200404'); // 清明連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200501'); // 勞動節
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200620'); // 補班日
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200625'); // 端午連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200626'); // 端午連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20200926'); // 補班日
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20201001'); // 中秋連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20201002'); // 中秋連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20201009'); // 國慶連假
+        $this->assertFalse($ret);
+        $ret = $obj->isMarketOpen('20201010'); // 國慶連假
+        $this->assertFalse($ret);
+    }
+    
+    /**
+     * 計算交易日
+     */
+    public function testCalculateMarketDays() {
+        $obj = new MarketDaysService();
+        
+        // 中間沒放假
+        $new_date = $obj->calculateMarketDays('20200622', '+', 1);
+        $this->assertEquals('20200623', $new_date);
+        $new_date = $obj->calculateMarketDays('20200617', '-', 2);
+        $this->assertEquals('20200615', $new_date);
+        
+        // 補班日不開盤
+        $new_date = $obj->calculateMarketDays('20200618', '+', 2);
+        $this->assertEquals('20200622', $new_date);
+        $new_date = $obj->calculateMarketDays('20200623', '-', 3);
+        $this->assertEquals('20200618', $new_date);
+        
+        // 連假不開盤
+        $new_date = $obj->calculateMarketDays('20200624', '+', 1);
+        $this->assertEquals('20200629', $new_date);
+        $new_date = $obj->calculateMarketDays('20200630', '-', 2);
+        $this->assertEquals('20200624', $new_date);
+        
+        // 假期中找下一個交易日
+        $new_date = $obj->calculateMarketDays('20200626', '-', 1);
+        $this->assertEquals('20200624', $new_date);
+        $new_date = $obj->calculateMarketDays('20200625', '+', 1);
+        $this->assertEquals('20200629', $new_date);
+    }
+}
