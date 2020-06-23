da13f17594a77b033a76c07d8dbcf3ace0f7a744
Tsukino Usagi <tsukinousagi@users.noreply.github.com>
2020-06-23 23:04:16 +0800

    install laravel log viewer

--- src/config/app.php
+++ src/config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // custom
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
+
     ],
 
     /*
