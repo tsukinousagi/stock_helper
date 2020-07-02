ae261d18f1e618db19a99c04b5a41eb2f4872f08
Tsukino Usagi <tsukinousagi@users.noreply.github.com>
2020-07-02 16:31:33 +0800

    取各種價量的service和test

--- src/app/Services/GoodsGraphService.php
+++ src/app/Services/GoodsGraphService.php
@@ -37,55 +37,72 @@ class GoodsGraphService {
      * @return number
      */
     public function getGoodGraphData(string $good, string $d_date, GoodsGraphDataType $data_type) {
-        // 如果是開盤價、最高價、最低價、收盤價、成交量，直接去db撈
-        if (in_array($data_type->value, [
-            GoodsGraphDataType::PriceOpen,
-            GoodsGraphDataType::PriceHigh,
-            GoodsGraphDataType::PriceLow,
-            GoodsGraphDataType::PriceClose,
-            GoodsGraphDataType::Volume,
-        ])) {
+        try {
+
             // 先去redis撈有沒有此價量
             $redis_key = 'GGD' . $good . $d_date . 'GDT' . $data_type;
             $field_data = $this->obj_redis->get($redis_key);
             if ($field_data == null) {
-                // 先去redis撈看看有沒有這筆「完整」的價量
-                $redis_row_key = 'P' . $good . $d_date;
-                $row_data = $this->obj_redis->get($redis_row_key);
-                if ($row_data == null) {
-                    // 去db撈並寫進redis
-                    $row_data = $this->getPriceFromDB($good, $d_date);
-                    $this->obj_redis->set($redis_row_key, serialize($row_data));
-                } else {
-                    $row_data = unserialize($row_data);
-                }
-                
-                // 取特定價量欄位
-                $this_field = $this->field_map[$data_type->value];
-                $field_data = $row_data[$this_field];
-                
-                // 成交量四捨五入
-                if ($data_type->value == GoodsGraphDataType::Volume) {
-                    $field_data = round($field_data);
-                }
-                $this->obj_redis->set($redis_key, $field_data);
+                // 如果是開盤價、最高價、最低價、收盤價、成交量，直接去db撈
+                if (in_array($data_type->value, [
+                    GoodsGraphDataType::PriceOpen,
+                    GoodsGraphDataType::PriceHigh,
+                    GoodsGraphDataType::PriceLow,
+                    GoodsGraphDataType::PriceClose,
+                    GoodsGraphDataType::Volume,
+                ])) {
+                    // 先去redis撈看看有沒有這筆「完整」的價量
+                    $redis_row_key = 'P' . $good . $d_date;
+                    $row_data = $this->obj_redis->get($redis_row_key);
+                    if ($row_data == null) {
+                        // 去db撈並寫進redis
+                        $row_data = $this->getPriceFromDB($good, $d_date);
+                        $this->obj_redis->set($redis_row_key, serialize($row_data));
+                    } else {
+                        $row_data = unserialize($row_data);
+                    }
+                    
+                    // 取特定價量欄位
+                    $this_field = $this->field_map[$data_type->value];
+                    $field_data = $row_data[$this_field];
+                    
+                    // 成交量四捨五入
+                    if ($data_type->value == GoodsGraphDataType::Volume) {
+                        $field_data = round($field_data);
+                    }
 
+                } else if ($data_type->value == GoodsGraphDataType::Price5MA) {
+                    // 5日均價
+                    $field_data = $this->getGoodMAPrice($good, $d_date, 5);
+                } else if ($data_type->value == GoodsGraphDataType::Price10MA) {
+                    // 10日均價
+                    $field_data = $this->getGoodMAPrice($good, $d_date, 10);
+                } else if ($data_type->value == GoodsGraphDataType::Price20MA) {
+                    // 20日均價
+                    $field_data = $this->getGoodMAPrice($good, $d_date, 20);
+                } else if ($data_type->value == GoodsGraphDataType::Price5MADirection) {
+                    // 5日均價方向
+                    $field_data = $this->getGoodMADirection($good, $d_date, 5);
+                } else if ($data_type->value == GoodsGraphDataType::Price10MADirection) {
+                    // 10日均價方向
+                    $field_data = $this->getGoodMADirection($good, $d_date, 10);
+                } else if ($data_type->value == GoodsGraphDataType::Price20MADirection) {
+                    // 20日均價方向
+                    $field_data = $this->getGoodMADirection($good, $d_date, 20);
+                }
+                // 寫入redis前先檢查
+                if (isset($field_data) && ($field_data <> null)) {
+                    $this->obj_redis->set($redis_key, $field_data);
+                } else {
+                    throw new Exception('取不到價量資料' . serialize($good) . serialize($d_date) . serialize($data_type));
+                }
             }
-            // todo 均價部份也用redis快取
-        // 5日均價
-        } else if ($data_type->value == GoodsGraphDataType::Price5MA) {
-            return $this->getGoodMAPrice($good, $d_date, 5);
-        // 10日均價
-        } else if ($data_type->value == GoodsGraphDataType::Price10MA) {
-            return $this->getGoodMAPrice($good, $d_date, 10);
-        // 20日均價
-        } else if ($data_type->value == GoodsGraphDataType::Price20MA) {
-            return $this->getGoodMAPrice($good, $d_date, 20);
-        } else {
+            
+            return $field_data;
+        } catch (Exception $e) {
+            Log::error($e->getLine() . ' ' . __CLASS__ . ':' . __FUNCTION__ . ' ' . $e->getMessage());
             return false;
         }
-        
-        return $field_data;
     }
     
 
@@ -105,12 +122,12 @@ class GoodsGraphService {
 
             // 查詢每日收盤價
             $remaining_days = $ma_days;
-            $next_day = $d_date;
+            $previous_day = $d_date;
             while($remaining_days > 0) {
-                $price_each_day[(string) $next_day] = (float) $this->getGoodGraphData($good, $next_day, GoodsGraphDataType::PriceClose());
+                $price_each_day[(string) $previous_day] = (float) $this->getGoodGraphData($good, $previous_day, GoodsGraphDataType::PriceClose());
                 // 查上一個交易日
-    //            var_dump($next_day);
-                $next_day = $obj_days->calculateMarketDays($next_day, '-', 1);
+    //            var_dump($previous_day);
+                $previous_day = $obj_days->calculateMarketDays($previous_day, '-', 1);
                 $remaining_days--;
             }
 
@@ -143,7 +160,25 @@ class GoodsGraphService {
      * @return \App\Enums\GoodsGraphMADirection
      */
     public function getGoodMADirection(string $good, string $d_date, int $ma_days) {
-        return GoodsGraphMADirection::MADUp();
+        $obj_days = new MarketDaysService();
+        // 取指定日期的均價
+        $this_ma = $this->getGoodMAPrice($good, $d_date, $ma_days);
+        // 取前一個交易日
+        $previous_day = $obj_days->calculateMarketDays($d_date, '-', 1);
+        // 取前一個交易日的均價
+        $previous_ma = $this->getGoodMAPrice($good, $previous_day, $ma_days);
+        // 比對
+        if ($previous_ma < $this_ma) {
+            $direction = GoodsGraphMADirection::MADUp();
+        } else if ($previous_ma > $this_ma) {
+            $direction = GoodsGraphMADirection::MADDown();
+        } else if ($previous_ma == $this_ma) {
+            $direction = GoodsGraphMADirection::MADFlat();
+        } else {
+            $direction = false;
+        }
+        
+        return $direction;
     }
     
     private function getPriceFromDB(string $good, string $d_date) {
