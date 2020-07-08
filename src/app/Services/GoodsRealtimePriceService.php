2e6052fc5a9c6307f9d71a2d08797c7109554a9a
Tsukino Usagi <tsukinousagi@users.noreply.github.com>
2020-07-08 20:09:45 +0800

    調整API回應寫入路徑

--- src/app/Services/GoodsRealtimePriceService.php
+++ src/app/Services/GoodsRealtimePriceService.php
@@ -11,6 +11,7 @@ use App\Services\StockGoodsService;
 use App\Services\MarketDaysService;
 use App\Enums\GoodsGraphDataType;
 use App\Services\FugleAPIService;
+//use App\Repositories\PricesMinuteRepository;
 use Illuminate\Support\Facades\Storage;
 
 
@@ -33,15 +34,41 @@ class GoodsRealtimePriceService {
      * @return string
      */
     public function getGoodsRealtimeData() {
+//        $obj_price_minute = new PricesMinuteRepository();
         // 目前先為了程式開發方便，先直接打API並存成文字檔，做模擬用的API
         $goods = ['2417', '2498', '2520', '4979', '1785', '1402', '2504', '3711', '2317', '4142'];
         
         foreach($goods as $v) {
             // 存檔路徑
-            $save_to = sprintf('fugle/%s/%s.txt', $v, date('YmdHi'));
+            $save_to = sprintf('fugle/%s/%s/%s/%s/%s.txt', $v, date('Y'), date('m'), date('d'), date('YmdHi'));
 
             // 打api
             $ret = $this->getGoodsRealtimeDataByGoods($v);
+            
+            /*
+            // test begin
+            $data = json_decode($ret, true);
+            $part = array_slice($data['data']['chart'], -2);
+            foreach($part as $k2 => $v2) {
+                // 取本地時間
+                $dt_local = date('Y-m-d H:i:s', strtotime($k2));
+                
+                // 準備塞db的資料
+                $row = [
+                    'code' => $v,
+                    'd_date' => $dt_local,
+                    'price_open' => $v2['open'],
+                    'price_high' => $v2['high'],
+                    'price_low' => $v2['low'],
+                    'price_close' => $v2['close'],
+                    'volume' => ($v2['volume'] / 1000),
+                ];
+                
+                $obj_price_minute->updatePrices($row);
+            }
+            */
+            
+            // test end
 
             // 存檔
             $ret2 = Storage::put($save_to, $ret);
