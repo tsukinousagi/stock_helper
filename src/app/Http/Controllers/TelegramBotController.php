8685d3e48a53f81d5e1ed7bd4e9648f58c1aedab
Tsukino Usagi <tsukinousagi@users.noreply.github.com>
2020-07-11 13:36:00 +0800

    log

--- src/app/Http/Controllers/TelegramBotController.php
+++ src/app/Http/Controllers/TelegramBotController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Telegram;
+use Log;
 
 class TelegramBotController extends Controller
 {
     //
     public function webhook(Request $request) {
         $updates = Telegram::getWebhookUpdates();
-        
+        Log::info(__CLASS__ . ':' . __FUNCTION__);
+        Log::info($updates);
         return 'ok';
     }
 }
